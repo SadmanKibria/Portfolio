b9cec9d1c0db49edea5a31251e9c9f49592a4680
Sadman Kibria <sadmankibria1@gmail.com>
2025-03-11 16:13:10 +0000

    apostrophe missing fixed

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -13,7 +13,7 @@ export default function NotFound() {
             <div className='sm:ml-6'>
               <div className='sm:border-l sm:border-gray-200 sm:pl-6'>
                 <h1 className='text-3xl font-bold tracking-tight sm:text-5xl'>
-                  I can't find the Page
+                  I can&apos;t find the Page
                 </h1>
                 <p className='mt-1 text-base text-gray-500'>
                   Please check the URL in the address bar and try again.
